1c8d942ca79d9d77ac3982b35364f74a191f7dc8
dsmtE <desmet.enguerrand@gmail.com>
2019-12-13 22:39:19 +0100

    Add the first two main files

---
+++ src/Smoothimacker.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+// SDL
+#include <SDL.h>
+
+// IMGUI
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/imgui_impl_sdl.h>
+
+// GLAD
+#include <glad/glad.h>
+
+// GLM & glm transform
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main(int argc, char *argv[]) {
+    
+  std::cout << "---------- Smoothimacker ----------" << std::endl;
+
+  return 0;
+}
